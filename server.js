bcf5f1c256fa554f28cecd0f4483687182576d14
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-05-31 01:24:43 +0200

    refactor callbacks to async/await

--- server.js
+++ server.js
@@ -22,56 +22,73 @@ app.get('/', (req, res) => {
 server.listen(port, () => console.log(`Server started on port ${port}`))
 
 io.on('connection', (socket) => {
-  socket.on('containers.list', () => {
+  socket.on('containers.list', async () => {
     refreshContainers()
   })
-  socket.on('container.start', (args) => {
+  socket.on('container.start', async (args) => {
     const {id} = args
     const container = docker.getContainer(id)
     if (container) {
-      container.start((err, data) => {
+      try {
+        await container.start()
         refreshContainers()
-      })
-    }
-  })
-  socket.on('container.stop', (args) => {
-    const {id} = args
-    const container = docker.getContainer(id)
-    if (container) {
-      container.stop((err, data) => {
-        refreshContainers()
-      })
-    }
-  })
-  socket.on('container.remove', (args) => {
-    const {id} = args
-    const container = docker.getContainer(id)
-    container.stop((err, data) => {
-      container.remove((err, data) => {
-        refreshContainers()
-      })
-    })
-  })
-  socket.on('image.run', (args) => {
-    const {name} = args
-    docker.createContainer({Image: name}, (err, container) => {
-      if (!err) {
-        container.start((err, data) => {
-          if (err) {
-            socket.emit('image.error', {message: err})
-          }
-        })
-      } else {
-        socket.emit('image.error', {message: err})
+      } catch (e) {
+        emitError(e)
       }
-    })
+    }
   })
+  socket.on('container.stop', async (args) => {
+    const {id} = args
+    const container = docker.getContainer(id)
+    if (container) {
+      try {
+        await container.stop()
+        refreshContainers()
+      }
+      catch (e) {
+        emitError(e)
+      }
+    }
+  })
+  socket.on('container.remove', async (args) => {
+    const {id} = args
+    const container = docker.getContainer(id)
+    if (container) {
+      try {
+        const info = await container.inspect()
+        if (info.State.Status === 'running') {
+          await container.stop()
+        }
+        await container.remove()
+        refreshContainers()
+      } catch (e) {
+        emitError(e)
+      }
+    }
+  })
+  socket.on('image.run', async (args) => {
+    const {name} = args
+    try {
+      const container = await docker.createContainer({Image: name})
+      await container.start()
+      refreshContainers()
+    } catch (e) {
+      emitError(e)
+    }
+  })
+
+  const emitError = (err) => {
+    socket.emit('image.error', {message: err})
+  }
 })
 
-const refreshContainers = () => {
-  docker.listContainers({all: true}, (err, containers) => {
+const refreshContainers = async () => {
+  try {
+    const containers = await docker.listContainers({all: true})
     io.emit('containers.list', containers)
-  })
+  } catch (e) {
+    emitError(e)
+  }
 }
 
 setInterval(refreshContainers, 1000)
